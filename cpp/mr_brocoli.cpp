a050448ebcc62ede67cb3adcb5ab2d2b12d43409
Ali Haciali <ahmthaciali@gmail.com>
2020-04-05 15:53:59 +0300

    compile error.txt

--- cpp/mr_brocoli.cpp
+++ cpp/mr_brocoli.cpp
@@ -1,2 +1,16 @@
-print helloword
-
+// Simple C++ program to display "Hello World" 
+  
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
